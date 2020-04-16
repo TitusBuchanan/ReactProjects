0897b7af13dab9699e7a38c3db736b1f45ade574
titus buchanan <titusbuchananjr@gmail.com>
2020-04-15 20:55:56 -0400

    cleaned up component to render in the virtual dom faster

--- App.js
+++ App.js
@@ -1,32 +1,40 @@
-import React, { Component } from "react";
-import "./App.css";
-import Resource from "./components/Resource";
-import resources from "./mock/resource";
+import React, { Component } from 'react';
+import './App.css';
+import Resource from './components/Resource'
+
+
 
 class App extends Component {
-  //[<Resource resource={aResource}/>, <Resource resource={aResource}]
-  // constructor(props) {
-  //   super(props);
-  //   this.renderPosts.bind(this);
-  // }
-  // Resource = require("./components/Resource");
-
-  renderPosts = () => {
-    const display = resources.map((resource) => {
-      return <Resource resource={resource} />;
-    });
-
-    return display;
-  };
-
-  render() {
+   resource = {
+    posterName:'Cliff',
+    resourceAuthor:'NetNinja',
+    authorSkillLevel:'Advanced',
+    cohort:'0',
+    title:'React & Redux Complete',
+    categories:['React','Redux'],
+    summary:'a good resource on building a couple of react apps with and without redux',
+    link:'www.google.com/search?=NetNinja%20React',
+    resourceType:'Playlist',
+    datePublished:'2018-09-10.00:00:00.000z',
+    videoLength:10,
+    timeToComplete:300,
+    rating:5,
+    comments:[{user:'Joe',text:'Pretty good resource'},{user:'Larry',text:'Pretty Pretty Pretty good resource'}]
+}
+  render(){
     return (
       <div className="App">
-        <h1>Welcome to BrainHive!</h1>
-        {this.renderPosts()}
+        <h1>Welcome To BrainHive</h1> 
+        <Resource resource={this.resource} />
+        <Resource resource={this.resource}/>
+        <Resource resource={this.resource}/>
+
       </div>
     );
   }
+  
 }
+ 
 
 export default App;
+ 
--- components/Resource.js
+++ components/Resource.js
@@ -1,33 +1,18 @@
 import React from 'react'
 
-const Resource = () => {
-    const resource = {
-        posterName:'Cliff',
-        resourceAuthor:'NetNinja',
-        authorSkillLevel:'Advanced',
-        cohort:'0',
-        title:'React & Redux Complete',
-        categories:['React','Redux'],
-        summary:'a good resource on building a couple of react apps with and without redux',
-        link:'www.google.com/search?=NetNinja%20React',
-        resourceType:'Playlist',
-        datePublished:'2018-09-10.00:00:00.000z',
-        videoLength:null,
-        timeToComplete:300,
-        rating:5,
-        comments:[{user:'Joe',text:'Pretty good resource'}]
-    }
+const Resource = ({resource}) => {
+   
     return(
         <div className={'box'}>
             <h2>{resource.title}</h2>
-            <p>{resource.resourceAuthor}</p>
-            <p>{resource.datePublished}</p>
-            {   resource.videoLength ? (<p>{resource.videoLength}</p>) : null}
+            <p>{resource.author}</p>
+            <p>Published On: {resource.datePublished}</p>
+            {resource.videoLength ? (<p>Length: {resource.videoLength} minutes</p>) : null }
+            {/*conditonal ? true code : false code */}
             <p>Comments: {resource.comments.length}</p>
-            <p>link:{resource.link}</p>
-            
         </div>
+
     )
 }
 
-export default Resource
+export default Resource 
