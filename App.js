81d9e2f59b99622d2ec3006043fa47ea033c8675
titus buchanan <titusbuchananjr@gmail.com>
2020-04-22 00:06:24 -0400

    more css

--- App.js
+++ App.js
@@ -20,7 +20,9 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        <h1 className="title">Welcome To BrainHive</h1> 
+          <div className="header">
+            <h1 className="title">Welcome To BrainHive!</h1> 
+          </div>
         <div className="resourceList"> {this.renderPosts()}</div>
       </div>
 
