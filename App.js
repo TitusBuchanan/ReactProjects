8f7d3c20e7ebace82c5de680cc0666d8c8133944
titus buchanan <titusbuchananjr@gmail.com>
2020-04-25 23:20:54 -0400

    added a form

--- App.js
+++ App.js
@@ -3,14 +3,21 @@ import './App.css';
 import Resource from './components/Resource'
 import  resources from './mock/resource'
 
+import ResourceForm from './components/ResourceForm'
+
+
+
 
 
 class App extends Component {
   
+  state = {
+    resources: [...resources]
+  }
 
   renderPosts= ()  => {
-    const display = resources.map(resource => {
-      return <Resource resource={resource} />;
+    const display = this.state.resources.map(resource => {
+      return <Resource resource={resource} key={resource.title} />;
     });
     
     return display
@@ -23,10 +30,11 @@ class App extends Component {
           <div className="header">
             <h1 className="title">Welcome To BrainHive!</h1>
               <div className="navigation">
-                <a href="">Add Post</a>
+                <a href="">Add Resource</a>
               </div>
           </div>
         <div className="resourceList"> {this.renderPosts()}</div>
+        <ResourceForm />
       </div>
 
 
